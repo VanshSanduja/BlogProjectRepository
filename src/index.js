073ceb2aee928bb0fb32475e7e82a343af71960d
VanshSanduja <vanshsanduja2@gmail.com>
2024-06-03 13:12:31 +0530

    Create to find a prime number

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 console.log("Hello World");
+
+let x = 17;
+
+var isPrime = true;
+
+for (let i = 2; i < x; i++) {
+  if (x % i == 0) isPrime = false;
+}
+if (isPrime == false) console.log(`${x} is not a prime Number`);
+else console.log(`${x} is a prime number`);
