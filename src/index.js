3736bd0d95c0318bace5ef6c4e695cfe84c87aca
VanshSanduja <vanshsanduja2@gmail.com>
2024-06-05 13:24:18 +0530

    Questions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,121 @@
-console.log("Hello World");
+// VANSH SANDUJA
 
-let x = 17;
+// Question 1
 
-var isPrime = true;
+// function printNames(students) {
 
-for (let i = 2; i < x; i++) {
-  if (x % i == 0) isPrime = false;
+//   for (let i = 0; i < students.length; i++) {
+
+//     console.log(students[i]);
+//   }
+
+// }
+
+// const students = [
+
+//   {name: "Vansh", grade: "A"},
+//   {name: "Ayush", grade: "A+"}
+// ];
+
+// printNames(students);
+
+// Question 2
+
+// var alpahabet = 'c';
+
+// switch(alpahabet) {
+
+//   case 'a' :
+
+//   console.log(`${alpahabet} This is a vowel!!`);
+//   break;
+
+//   case 'e' :
+
+//   console.log(`${alpahabet} This is a vowel!!`);
+//   break;
+
+//   case 'i' :
+
+//   console.log(`${alpahabet} This is a vowel!!`);
+//   break;
+
+//   case 'o' :
+
+//   console.log(`${alpahabet} This is a vowel!!`);
+//   break;
+
+//   case 'u' :
+
+//   console.log(`${alpahabet} This is a vowel!!`);
+//   break;
+
+//   default:
+
+//   console.log("Not a vowel!!");
+
+// }
+
+// Question 3
+
+// function printPerson(person) {
+
+//   console.log(`${person.firstName} ${person.lastName}`);
+//   console.log(`${person.firstName1} ${person.lastName1}`);
+// }
+
+// const person = {
+
+//   firstName: "Vansh",
+//   lastName: "Sanduja",
+
+//   firstName1: "Sanduja",
+//   lastName1: "Vansh"
+
+// };
+
+// printPerson(person);
+
+// Question 4
+
+// var celsius = 25;
+
+// var fahrenheit = (celsius * 9 / 5) + 32;
+
+// var temperature = fahrenheit;
+
+// if (temperature < 60) {
+
+//   console.log("too cold");
+// }
+
+// else if (temperature > 60 && temperature < 85) {
+
+//   console.log("Normal Temperature");
+// }
+
+// else if (temperature > 85){
+
+//   console.log("very hot");
+// }
+
+let givenArray = [2, 4, 6, 8, 10];
+
+let target = 10;
+
+let num1 = 0;
+let num2 = 0;
+
+for (let i = 0; i < givenArray.length; i++) {
+
+    for (let j = 1; j < givenArray.length; j++) {
+
+        if(givenArray[i] + givenArray[j] == target) {
+
+            num1 = givenArray[i];
+            num2 = givenArray[j];
+        }
+    }
 }
-if (isPrime == false) console.log(`${x} is not a prime Number`);
-else console.log(`${x} is a prime number`);
+
+console.log(`${num1} ${num2}`);
